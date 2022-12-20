51bad98d085c15fe30a0030aa4babe2aa5ea8e91
JeebsM <marchaljeebs@gmail.com>
2022-12-20 10:27:55 +0100

    HM-1: import of custom HTML elements

---
+++ src/views/components.js
@@ -0,0 +1,3 @@
+import navigation from '../components/navigation.js';
+import authentication from '../components/authentication.js';
+import IndexRecordForm from '../components/index-record-form.js';
